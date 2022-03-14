fab62d43747b9cbf2a54da89e3668247b4273da0
Hari2019041 <hari19041@iiitd.ac.in>
2022-03-14 12:41:35 +0530

    Added needle show and end point method

--- main.py
+++ main.py
@@ -5,12 +5,17 @@ import math
 pygame.init()
 pygame.font.init()
 
-WIDTH = 900
+WIDTH = 1000
 HEIGHT = 600
 MARGIN = 50
 TITLE = 'Buffon\'s Needle Problem'
 ICON = pygame.image.load('icon.png')
 SCREEN = pygame.display.set_mode((WIDTH, HEIGHT))
+
+# Colors
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
 
 
 def set_window(TITLE, ICON):
@@ -27,11 +32,26 @@ class Simulation:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     self.RUNNING = False
+            pygame.display.update()
 
 
 class Needle:
-    def __init__(self):
-        pass
+    def __init__(self, x, y, angle, color):
+        self.length = 50
+        self.x = x
+        self.y = y
+        self.start = (x, y)
+        self.angle = angle
+        self.end = self.find_end_point()
+        self.color = color
+
+    def find_end_point(self):
+        new_x = self.x + self.length*math.cos(self.angle)
+        new_y = self.y - self.length*math.sin(self.angle)
+        return (new_x, new_y)
+
+    def show(self):
+        pygame.draw.line(SCREEN, WHITE, self.start, self.end)
 
 
 class Floor:
